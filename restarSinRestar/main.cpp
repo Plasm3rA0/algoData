6b795978da8a2f273bfafc3340a9151ffad2fa41
Plasm3rA0 <p.hernan.camino2005@gmail.com>
2023-05-23 09:25:00 +0200

    Tercer commit

--- restarSinRestar/main.cpp
+++ restarSinRestar/main.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+//! Bad way to make it
+
+/*
 int main(){
     int a = 5,b = 1, c = 0, initialB = b;
     while (a > b){
@@ -9,5 +12,26 @@ int main(){
         c++;
     }
     printf("La resta sin restar de a - b osea %i - %i és %i",a,initialB,c);
+    return 0;
+}
+*/
+
+int subAlg(int a, int b){
+    int c = 0;
+    if(a < b){
+        c = a;
+        a = b;
+        b = c;
+        c = 0;
+    }
+    while( a > b){
+        b++;
+        c++;
+    }
+    return c;
+}
+
+int main(){
+    printf("5 - 1 = %i",subAlg(5,1));
     return 0;
 }
