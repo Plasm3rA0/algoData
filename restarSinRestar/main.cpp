b3793e3c88812d04bcf9c9c8e8172975f12bafcb
Plasm3rA0 <p.hernan.camino2005@gmail.com>
2023-05-23 09:19:55 +0200

    Segundo commit

--- restarSinRestar/main.cpp
+++ restarSinRestar/main.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 
-int main(){
-    
+using namespace std;
 
+int main(){
+    int a = 5,b = 1, c = 0, initialB = b;
+    while (a > b){
+        b++;
+        c++;
+    }
+    printf("La resta sin restar de a - b osea %i - %i és %i",a,initialB,c);
     return 0;
 }
